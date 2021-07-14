62070407aa6241589616f31f5c6b81d0c864fa83
ZenithalHourlyRate <i@zenithal.me>
2021-07-15 01:56:01 +0800

    Add dryrun and quiet

--- stars.sh
+++ stars.sh
@@ -4,6 +4,24 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
+# parse arguments
+DRYRUN=0
+QUIET=0
+
+while [[ $# -gt 0 ]]; do
+  key="$1"
+  case $key in
+    -d|--dryrun)
+      DRYRUN=1
+      shift
+      ;;
+    -q|--quiet)
+      QUIET=1
+      shift
+      ;;
+  esac
+done
+
 # sanity check
 function check() {
   for com in $1; do
@@ -21,17 +39,20 @@ list=""
 
 ## Arch Linux
 check "pacman xargs grep sed cut sort uniq head"
-if [ $? -eq 0 ]; then
+if [[ $? -eq 0 ]]; then
   list="$(pacman -Qq | xargs pacman -Qi | grep URL | grep 'https://github.com/' | sed 's/  */|/g' | cut -d '|' -f 3 | sed -E 's_https://github.com/([-_\.a-zA-Z0-9]+)/([-_\.a-zA-Z0-9]+).*_\1/\2_' | sed '/:/d' | sort | uniq) $list"
 fi
 
 # star!
 for repo in $list; do
-  echo -n "Starring github.com/$repo ..."
-  echo $repo | sed "s_^_/user/starred/_" | xargs echo -n
+  com="gh"
+  if [[ $DRYRUN -eq 1 ]]; then
+    com="echo"
+  fi
+  echo $repo | sed "s_^_/user/starred/_" | xargs $com > /dev/null
   if [[ $? -ne 0 ]]; then
-    echo " ${RED}error!${NC}"
-  else
-    echo " done"
+    echo -e "${RED}! Error when star github.com/$repo${NC}"
+  elif [[ $QUIET -ne 1 ]]; then
+    echo -e "Starred ${repo}"
   fi
 done
