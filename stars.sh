8ff56f2c5ac2ed050f21bfba66c8298bd21b1d6d
ZenithalHourlyRate <i@zenithal.me>
2021-07-15 03:13:33 +0800

    Add yum

--- stars.sh
+++ stars.sh
@@ -12,6 +12,7 @@ BURST=0
 TARGET=0
 PACMAN=0
 DPKG=0
+YUM=0
 
 while [[ $# -gt 0 ]]; do
   case "$1" in
@@ -28,7 +29,7 @@ while [[ $# -gt 0 ]]; do
       BURST=1
       shift
       ;;
-    pacman|archlinux)
+    pacman|archlinux|manjaro)
       echo "Argument $1: pacman module enabled"
       TARGET=1
       PACMAN=1
@@ -39,6 +40,17 @@ while [[ $# -gt 0 ]]; do
       TARGET=1
       DPKG=1
       shift
+      ;;
+    yum|centos|fedora)
+      echo "Argument $1: yum module enabled"
+      TARGET=1
+      YUM=1
+      shift
+      ;;
+    *)
+      echo "Unknown argument: $1"
+      exit 1
+      ;;
   esac
 done
 
@@ -66,7 +78,7 @@ function extract() {
   sed -E 's_https://github.com/([-_\.a-zA-Z0-9]+)/([-_\.a-zA-Z0-9]+).*_\1/\2_' | sed '/:/d' | sort | uniq
 }
 
-## Arch Linux
+## Arch Linux/Manjaro etc
 if [[ $PACMAN -eq 1 ]]; then
   check "pacman cut"
   list="$(pacman -Qq | xargs pacman -Qi | grep URL | grep 'https://github.com/' | sed 's/  */|/g' | cut -d '|' -f 3 | extract) $list"
@@ -76,6 +88,12 @@ fi
 if [[ $DPKG -eq 1 ]]; then
   check "dpkg-query"
   list="$(dpkg-query -f '${Homepage}\n' -W | grep 'https://github.com/' | extract) $list"
+fi
+
+## CentOS/Fedora etc
+if [[ $YUM -eq 1 ]]; then
+  check "yum"
+  list="$(yum info installed 2> /dev/null | grep URL | grep 'https://github.com/' | sed 's/  */|/g' | cut -d '|' -f 3 | extract) $list"
 fi
 
 ## No target
