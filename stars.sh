2ea9cad100354c6071a009afa548e2d3aa42a105
ZenithalHourlyRate <i@zenithal.me>
2021-07-15 02:59:01 +0800

    Add dpkg

--- stars.sh
+++ stars.sh
@@ -9,9 +9,12 @@ DRYRUN=0
 QUIET=0
 BURST=0
 
+TARGET=0
+PACMAN=0
+DPKG=0
+
 while [[ $# -gt 0 ]]; do
-  key="$1"
-  case $key in
+  case "$1" in
     -d|--dryrun)
       DRYRUN=1
       shift
@@ -25,15 +28,27 @@ while [[ $# -gt 0 ]]; do
       BURST=1
       shift
       ;;
+    pacman|archlinux)
+      echo "Argument $1: pacman module enabled"
+      TARGET=1
+      PACMAN=1
+      shift
+      ;;
+    dpkg|ubuntu|debian)
+      echo "Argument $1: dpkg module enabled"
+      TARGET=1
+      DPKG=1
+      shift
   esac
 done
 
 # sanity check
 function check() {
   for com in $1; do
-    which $com > /dev/null
+    which $com &> /dev/null
     if [ $? -ne 0 ]; then
-      return 1
+      echo -e "${RED}$com not found${NC}"
+      exit 1
     fi
   done
   return 0
@@ -41,15 +56,32 @@ function check() {
 
 # generate list
 list=""
-check "gh"
-if [[ $? -ne 0 ]]; then
-  exit 1
+if [[ $DRYRUN -eq 0 ]]; then
+  check "gh"
 fi
 
+check "grep sed sort uniq xargs"
+
+function extract() {
+  sed -E 's_https://github.com/([-_\.a-zA-Z0-9]+)/([-_\.a-zA-Z0-9]+).*_\1/\2_' | sed '/:/d' | sort | uniq
+}
+
 ## Arch Linux
-check "pacman xargs grep sed cut sort uniq head"
-if [[ $? -eq 0 ]]; then
-  list="$(pacman -Qq | xargs pacman -Qi | grep URL | grep 'https://github.com/' | sed 's/  */|/g' | cut -d '|' -f 3 | sed -E 's_https://github.com/([-_\.a-zA-Z0-9]+)/([-_\.a-zA-Z0-9]+).*_\1/\2_' | sed '/:/d' | sort | uniq) $list"
+if [[ $PACMAN -eq 1 ]]; then
+  check "pacman cut"
+  list="$(pacman -Qq | xargs pacman -Qi | grep URL | grep 'https://github.com/' | sed 's/  */|/g' | cut -d '|' -f 3 | extract) $list"
+fi
+
+## Debian/Ubuntu etc
+if [[ $DPKG -eq 1 ]]; then
+  check "dpkg-query"
+  list="$(dpkg-query -f '${Homepage}\n' -W | grep 'https://github.com/' | extract) $list"
+fi
+
+## No target
+if [[ $TARGET -eq 0 ]]; then
+  echo "No target(e.g. pacman, dpkg) specified"
+  exit 1
 fi
 
 # star!
