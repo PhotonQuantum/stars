a6b76e394846f2b3fc8f67b3d2714a3394101bc5
LightQuantum <self@lightquantum.me>
2022-02-11 00:43:36 +0800

    feat: custom portage impl

--- src/portage.rs
+++ src/portage.rs
@@ -1,15 +1,23 @@
 //! Portage integration.
 
 use std::collections::HashMap;
+use std::fs;
+use std::io::ErrorKind;
+use std::path::{Path, PathBuf};
 use std::process::Command;
 use std::str;
 use std::str::FromStr;
 
+use itertools::Itertools;
+use once_cell::sync::Lazy;
 use regex::Regex;
+use tap::TapOptional;
 use url::Url;
 
 use crate::common::{BoxedError, Package, Source, SourceType};
 use crate::{Logger, TargetRegistry};
+
+static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r#"(?m)^HOMEPAGE="(.+)"$"#).unwrap());
 
 #[derive(Debug)]
 pub struct Portage;
@@ -24,38 +32,91 @@ impl Source for Portage {
     }
 
     fn available(&self) -> bool {
-        which::which("equery").is_ok()
+        which::which("portageq").is_ok()
     }
 
     fn snapshot(
         &self,
-        _logger: &Logger,
+        logger: &Logger,
         _files: HashMap<&str, &[u8]>,
         targets: &TargetRegistry,
     ) -> Result<Vec<Package>, BoxedError> {
-        let re = Regex::new("Homepage: *(.+)").unwrap();
-
-        let raw_output = Command::new("equery").arg("list").arg("*").output()?.stdout;
+        let raw_output = Command::new("equery")
+            .arg("list")
+            .arg("*")
+            .arg("-F")
+            .arg("$repo;$category;$name;$fullversion")
+            .output()?
+            .stdout;
         let packages = str::from_utf8(&*raw_output)?;
+
+        let mut repo_path_cache = HashMap::new();
 
         Ok(packages
             .lines()
             .filter_map(|package| {
-                let (_, name) = package.split_once("/")?;
-                let raw_output = Command::new("equery")
-                    .arg("meta")
-                    .arg("-U")
-                    .arg(package)
-                    .output()
-                    .ok()?
-                    .stdout;
-                let metadata = str::from_utf8(&*raw_output).ok()?;
-                re.captures(metadata).and_then(|cap| {
-                    let homepage = &cap[1];
-                    targets.try_parse(name.to_string(), &Url::from_str(homepage).ok()?)
-                })
+                let (repo, category, name, full_version) = package.split(';').collect_tuple()?;
+                let repo_path = repo_path_cache
+                    .entry(repo.to_string())
+                    .or_insert_with(|| {
+                        repo_path(repo).tap_none(|| {
+                            logger.warn(format!("repo {} not found", repo));
+                        })
+                    })
+                    .as_ref()?;
+                let homepages = portage_homepage(repo_path, category, name, full_version)
+                    .unwrap_or_else(|e| {
+                        logger.warn(format!("package {}/{}: {}", category, name, e));
+                        None
+                    })?;
+                homepages
+                    .into_iter()
+                    .find_map(|homepage| targets.try_parse(name.to_string(), &homepage))
             })
             .collect())
+    }
+}
+
+fn repo_path(repo: &str) -> Option<PathBuf> {
+    PathBuf::from_str(
+        str::from_utf8(
+            &*Command::new("portageq")
+                .arg("get_repo_path")
+                .arg("/")
+                .arg(repo)
+                .output()
+                .ok()?
+                .stdout,
+        )
+        .ok()?
+        .trim(),
+    )
+    .ok()
+}
+
+fn portage_homepage(
+    repo_path: impl AsRef<Path>,
+    category: &str,
+    name: &str,
+    full_version: &str,
+) -> Result<Option<Vec<Url>>, BoxedError> {
+    let ebuild_path = repo_path
+        .as_ref()
+        .join(category)
+        .join(name)
+        .join(format!("{}-{}.ebuild", name, full_version));
+    match fs::read_to_string(ebuild_path) {
+        Ok(ebuild) => Ok(RE
+            .captures(ebuild.as_str())
+            .map(|cap| {
+                cap[1]
+                    .split(' ')
+                    .map(|homepage| Url::from_str(homepage).ok())
+                    .collect::<Option<Vec<Url>>>()
+            })
+            .unwrap_or_default()),
+        Err(e) if e.kind() == ErrorKind::NotFound => Ok(None),
+        Err(e) => Err(Box::new(e)),
     }
 }
 
@@ -66,7 +127,7 @@ mod tests {
     use super::Portage;
 
     #[test]
-    fn test_pacman() {
+    fn test_portage() {
         test_source(&Portage);
     }
 }
